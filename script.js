75eb433031ce5a92adb79070caae12e3cbdaf4a0
Luka Kikvidze <programming1725@gmail.com>
2025-01-15 00:15:13 +0400

    Css შესწორება

--- script.js
+++ script.js
@@ -2,15 +2,14 @@ const slider = document.querySelector('.slider');
 const nextBtn = document.getElementById('next-btn');
 const prevBtn = document.getElementById('prev-btn');
 
-let currentPosition = 0; // Track the current slide position
+let currentPosition = 0;
 
 function updateSlider() {
-  // Move the slider
   slider.style.transform = `translateX(${-currentPosition * 100}px)`;
 
   // Enable/disable buttons
   prevBtn.disabled = currentPosition === 0;
-  nextBtn.disabled = currentPosition === slider.children.length - 4; // Show 4 items at a time
+  nextBtn.disabled = currentPosition === slider.children.length - 4;
 }
 
 nextBtn.addEventListener('click', () => {
@@ -23,9 +22,7 @@ prevBtn.addEventListener('click', () => {
   updateSlider();
 });
 
-// Initialize the slider
 updateSlider();
-
 
 
 //main slider js 
@@ -83,10 +80,8 @@ function prevSlide() {
 
 //Language
 
-// JSON data for translation
 
-    // Define Georgian and English translations
-    const translations = {
+const translations = {
         ka: {
             "search-placeholder": "ძებნა",
             "language": "ქარ",
